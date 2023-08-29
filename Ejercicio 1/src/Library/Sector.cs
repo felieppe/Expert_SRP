590b7089c4bff3570c00ddd456c1290c373298da
Felipe <me@felieppe.com>
2023-08-29 10:44:53 -0300

    Functions added

--- Ejercicio 1/src/Library/Sector.cs
+++ Ejercicio 1/src/Library/Sector.cs
@@ -9,7 +9,24 @@ namespace SRP
         public Sector(string sector, Shelve shelve) {
             this.LibrarySector = sector;
             this.Shelve = shelve;
+        }
 
+        public string GetLibrarySector() {
+            return LibrarySector;
+        }
+        public Shelve GetShelve() {
+            return Shelve;
+        }
+
+        public void SetLibrarySector(string sector) {
+            if (!string.IsNullOrEmpty(sector)) {
+                this.LibrarySector = sector;
+            }
+        }
+        public void SetShelve(Shelve shelve) {
+            if (shelve != null) {
+                this.Shelve = shelve;
+            }
         }
     }
 }
--- Ejercicio 1/src/Library/Shelve.cs
+++ Ejercicio 1/src/Library/Shelve.cs
@@ -4,10 +4,28 @@ namespace SRP
 {
     public class Shelve {
         private string LibraryShelve { get ; set; }
-        private Book book { get; set; }
+        private Book Book { get; set; }
         public Shelve(string shelve, Book book) {
             this.LibraryShelve = shelve;
-            this.book = book;
+            this.Book = book;
+        }
+
+        public string GetLibraryShelve() {
+            return LibraryShelve;
+        }
+        public Book GetBook() {
+            return this.Book;
+        }
+
+        public void SetLibraryShelve(string shelve) {
+            if (!string.IsNullOrEmpty(shelve)) {
+                this.LibraryShelve = shelve;
+            }
+        }
+        public void SetBook(Book book) {
+            if (book != null) {
+                this.Book = book;
+            }
         }
     }
 }
