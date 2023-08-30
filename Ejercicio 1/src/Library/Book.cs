17c91ddcc64ece54e79d481198fa385311456946
Felipe <me@felieppe.com>
2023-08-30 08:14:45 -0300

    Removed comments

--- Ejercicio 1/src/Library/Book.cs
+++ Ejercicio 1/src/Library/Book.cs
@@ -4,17 +4,9 @@ namespace SRP
 {
     public class Book
     {
-        // Información del libro:                   TÍTULO
         private string Title { get ; }
-        // Información del libro:                   AUTOR
         private string Author { get ; }
-        // Información del libro:                   CÓDIGO DE IDENTIFICACIÓN
         private string Code { get ;  }
-
-        // Ubicación en la bibloteca:               SECTOR
-        //public string LibrarySector { get ; set; }
-        // Ubicación en la bibloteca:               ESTANTE
-        //public string LibraryShelve { get ; set; }
 
         public Book(String title, String author, String code)
         {
@@ -22,22 +14,5 @@ namespace SRP
             this.Author = author;
             this.Code = code;
         }
-
-        /*
-        
-            La responsabilidad de saber en que ubicación (sector y estante) no debería de recaer en la clase Book.
-            Los unicos datos importantes los cuales le corresponde a la clase Book es Title, Author y Code, los adicionales deberian de estar
-            comprendidos en otra clase. Este libro conoce información que no debería de conocer.
-
-            Propongo modificar la función ShelveBook deberia estar presentada como una clase aparte, la cual le enviariamos los datos Sector y Shelve.
-            Al Book le quitariamos la responsabilidad de saber su ubicacion (el sector y la shelve) y creariamos estos dos como clases.
-        
-        */ 
-        /*public void ShelveBook(String sector, String shelve)
-        {
-            this.LibrarySector = sector;
-            this.LibraryShelve = shelve;
-        }*/
-
     }
 }
