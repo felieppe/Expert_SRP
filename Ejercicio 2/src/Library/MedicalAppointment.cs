63560373cc1810fe98c2ce26d66b2a57dd020e24
felieppe <me@felieppe.com>
2023-08-30 12:16:02 -0300

    Unique ID parameter added to MedicalAppointment class

--- Ejercicio 2/src/Library/MedicalAppointment.cs
+++ Ejercicio 2/src/Library/MedicalAppointment.cs
@@ -2,15 +2,20 @@ using System;
 
 namespace Library {
     public class MedicalAppointment {
+        private string Id {get; set;}
         private DateTime Date {get; set;}
         private string AppointmentPlace {get; set;}
         private Doctor Doctor {get; set;}
-        public MedicalAppointment (DateTime date, string place, Doctor doc) {
+        public MedicalAppointment (string id, DateTime date, string place, Doctor doc) {
+            SetId(id);
             SetDateTime(date);
             SetAppointmentPlace(place);
             SetDoctor(doc);
         }
 
+        public string GetId() {
+            return this.Id;
+        }
         public DateTime GetDateTime() {
             return this.Date;
         }
@@ -21,6 +26,11 @@ namespace Library {
             return this.Doctor;
         }
 
+        public void SetId(string id) {
+            if (!string.IsNullOrEmpty(id)) {
+                this.Id = id;
+            }
+        }
         public void SetDateTime(DateTime dateTime) {
             if (dateTime != DateTime.MinValue) {
                 this.Date = dateTime;
--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -9,7 +9,7 @@ namespace Program
         {
             PacientInfo pacientInfo = new PacientInfo("Steven Jhonson", 18, "986782342", "5555-555-555");
             Doctor doctor = new Doctor("Armand", "");
-            MedicalAppointment medicalAppointment = new MedicalAppointment(DateTime.Now, "Wall Street", doctor);
+            MedicalAppointment medicalAppointment = new MedicalAppointment("0001", DateTime.Now, "Wall Street", doctor);
             Pacient pacient = new Pacient(pacientInfo);
             AppointmentService aps = new AppointmentService(pacient, medicalAppointment);
 
@@ -17,7 +17,7 @@ namespace Program
 
             PacientInfo pacientInfo2 = new PacientInfo("Ralf Manson", 25, "", "5555-555-555");
             Doctor doctor2 = new Doctor("", "");
-            MedicalAppointment medicalAppointment2 = new MedicalAppointment(DateTime.Now, "Queen Street", doctor);
+            MedicalAppointment medicalAppointment2 = new MedicalAppointment("0002", DateTime.Now, "Queen Street", doctor);
             Pacient pacient2 = new Pacient(pacientInfo2);
             AppointmentService aps2 = new AppointmentService(pacient2, medicalAppointment2);
 
