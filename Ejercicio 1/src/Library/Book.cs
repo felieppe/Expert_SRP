5d1070574a065426f91cc83f36544d6ae83b28aa
Felipe <me@felieppe.com>
2023-08-30 08:19:13 -0300

    Added SET/GET function

--- Ejercicio 1/src/Library/Book.cs
+++ Ejercicio 1/src/Library/Book.cs
@@ -4,15 +4,41 @@ namespace SRP
 {
     public class Book
     {
-        private string Title { get ; }
-        private string Author { get ; }
-        private string Code { get ;  }
+        private string Title { get; set;}
+        private string Author { get; set;}
+        private string Code { get; set;}
 
         public Book(String title, String author, String code)
         {
             this.Title = title;
             this.Author = author;
             this.Code = code;
+        }
+
+        public string GetTitle() {
+            return this.Title;
+        }
+        public string GetAuthor() {
+            return this.Author;
+        }
+        public string GetCode() {
+            return this.Code;
+        }
+
+        public void SetTitle(string title) {
+            if (!string.IsNullOrEmpty(title)) {
+                this.Title = title;
+            }
+        }
+        public void SetAuthor(string author) {
+            if (!string.IsNullOrEmpty(author)) {
+                this.Author = author;
+            }
+        }
+        public void SetCode(string code) {
+            if (!string.IsNullOrEmpty(code)) {
+                this.Code = code;
+            }
         }
     }
 }
